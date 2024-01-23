9ec17dfd410874b2944cd323040a465b97a1d0c5
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2024-01-23 16:24:34 +0500

    abubakir tuppoy

--- main.js
+++ main.js
@@ -107,6 +107,7 @@
        textarea.value = "";
        resetImagePositions()
      } else {
+       congratulations()
        level.textContent = "GAME OVER or LEVEL COMPLETED";
        task.innerHTML = "";
        rating.push(
@@ -269,17 +270,39 @@
 
  addData();
 
- function resetImagePositions() {
-  let images = document.querySelectorAll('.image');
-  images.forEach(image => {
-    image.style.transform = 'translate(0, 0)';
-    console.log(images);
-  });
-}
-function showName(){
-  let name = localStorage.getItem('nickname');
-  if(name){
-    nickName.value = name;
+ function congratulations () {
+  console.log('worked')
+  const duration = 15 * 1000,
+  animationEnd = Date.now() + duration,
+  defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
-  }
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
 }
+
+const interval = setInterval(function() {
+  const timeLeft = animationEnd - Date.now();
+
+  if (timeLeft <= 0) {
+    return clearInterval(interval);
+  }
+
+  const particleCount = 50 * (timeLeft / duration);
+
+  // since particles fall down, start a bit higher than random
+  confetti(
+    Object.assign({}, defaults, {
+      particleCount,
+      origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+    })
+  );
+  confetti(
+    Object.assign({}, defaults, {
+      particleCount,
+      origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+    })
+  );
+}, 250);
+ }
+
+congratulations()
